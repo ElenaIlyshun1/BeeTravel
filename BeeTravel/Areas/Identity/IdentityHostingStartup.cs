3f4126b128b89fa7cc7f623f4d0dbee199b7ed81
User <User@DESKTOP-JM8KOQL>
2020-11-04 12:55:19 +0200

    Identity

---
+++ BeeTravel/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BeeTravel.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BeeTravel.Areas.Identity.IdentityHostingStartup))]
+namespace BeeTravel.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BeeTravelContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BeeTravelContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BeeTravelContext>();
+            });
+        }
+    }
+}

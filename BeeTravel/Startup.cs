75e209533c84a5f93353aece60e2fe71432617b6
Vadym Pavlina <vadim.table.pc@gmail.com>
2020-11-05 15:57:26 +0200

    Finished Localization and Globalization

--- BeeTravel/Startup.cs
+++ BeeTravel/Startup.cs
@@ -12,6 +12,9 @@ using BeeTravel.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
 namespace BeeTravel
 {
@@ -36,6 +39,24 @@ namespace BeeTravel
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.Configure<RequestLocalizationOptions>(
+                opt =>
+                {
+                    var supportedCulteres = new List<CultureInfo>
+                    {
+                       new CultureInfo("en"),
+                       new CultureInfo("ru")
+                    };
+                    opt.DefaultRequestCulture = new RequestCulture("en");
+                    opt.SupportedCultures = supportedCulteres;
+                    opt.SupportedUICultures = supportedCulteres;
+
+                });
+
+            services.AddControllersWithViews()
+                .AddViewLocalization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +73,19 @@ namespace BeeTravel
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //var supportedCultures = new[]
+            //{
+            //    new CultureInfo("en"),
+            //    new CultureInfo("ru"),
+            //};
+            //app.UseRequestLocalization(new RequestLocalizationOptions
+            //{
+            //    DefaultRequestCulture = new RequestCulture("en"),
+            //    SupportedCultures = supportedCultures,
+            //    SupportedUICultures = supportedCultures
+            //});
             //app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -60,6 +93,8 @@ namespace BeeTravel
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseRequestLocalization(app.ApplicationServices.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(

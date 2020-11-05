433cc65ca6f3973ecf549b89a3b3f9a1da298ffb
User <User@DESKTOP-JM8KOQL>
2020-11-05 16:21:56 +0200

    change startup

--- BeeTravel/Areas/Identity/IdentityHostingStartup.cs
+++ BeeTravel/Areas/Identity/IdentityHostingStartup.cs
@@ -19,8 +19,8 @@ namespace BeeTravel.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("BeeTravelContextConnection")));
 
-                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<BeeTravelContext>();
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<BeeTravelContext>();
             });
         }
     }

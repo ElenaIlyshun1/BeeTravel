a681183e9f6c19ae922a0477f6f20966ef2c2481
User <User@DESKTOP-JM8KOQL>
2020-12-16 12:41:17 +0200

    Admin Page Tour Control

--- BeeTravel/Helpers/ImageHelper.cs
+++ BeeTravel/Helpers/ImageHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -27,6 +29,38 @@ namespace BeeTravel.Helpers
             }
             catch { return null; }
 
+        }
+        //Отримати повний шлях фото
+        public static string GetImagePath(IHostingEnvironment host, string imgName)
+        {
+            return Path.Combine(host.WebRootPath, "img", imgName);
+        }
+
+        //Збереження фото
+        public static string SaveImage(IHostingEnvironment host, IFormFile img)
+        {
+            string uniqImgName = null;
+
+            if (img != null)
+            {
+                //генеруємо нове імя
+                uniqImgName = Guid.NewGuid().ToString() + "_" + img.FileName;
+                //дістаємо повний шлях фото
+                string filePath = GetImagePath(host, uniqImgName);
+                //зберігаємо фото
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    img.CopyTo(fileStream);
+                }
+            }
+            return uniqImgName;
+        }
+
+        //Видалення фото
+        public static void DeleteImage(IHostingEnvironment host, string imgName)
+        {
+            string filePath = GetImagePath(host, imgName);
+            File.Delete(filePath);
         }
     }
 }
---
+++ BeeTravel/Models/AdminViewModels/TourEditVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeTravel.Models.AdminViewModels
+{
+    public class TourEditVM : TourCreateVM
+    {
+        public int Id { get; set; }
+        public string ExistImgName { get; set; }
+        public string ExistImgLargeName { get; set; }
+    }
+}

dbd38b1f9eaf75b1dadd410343093e771e059d83
vadimpavlina <vadim.table.pc@gmail.com>
2021-01-08 18:38:08 +0200

    Work on Edit Role

--- BeeTravel/Controllers/AdministrationController.cs
+++ BeeTravel/Controllers/AdministrationController.cs
@@ -285,7 +285,56 @@ namespace BeeTravel.Controllers
                 return View("RoleIndex");
             }
         }
-       
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+                RoleColor = role.RoleColor
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id.ToString());
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                role.RoleColor = model.RoleColor;
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("RoleIndex");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+        }
+
     }
 
 }

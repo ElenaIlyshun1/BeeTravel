fb8297bef7a66c88c90b9bd9c0e9b418644e19d8
vadimpavlina <vadim.table.pc@gmail.com>
2021-02-03 11:57:04 +0200

    Edit UserImage

---
+++ BeeTravel/Migrations/20210129104451_UserPhoto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeeTravel.Migrations
+{
+    public partial class UserPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+        }
+    }
+}

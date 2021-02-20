95aafb6fce439fc2a4356c9de0b00e8117a8b68f
vadimpavlina <vadim.table.pc@gmail.com>
2021-02-20 12:43:34 +0200

    Edit Style Profile Page and Order Page

---
+++ BeeTravel/Controllers/OrderController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using BeeTravel.Entities;
+using BeeTravel.Helpers;
+using BeeTravel.Interfaces;
+using BeeTravel.Models;
+using BeeTravel.Models.AccountViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace BeeTravel.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly UserManager<DbUser> _userManager;
+        private readonly SignInManager<DbUser> _signInManager;
+        private readonly RoleManager<DbRole> _roleManager;
+        private readonly IWebHostEnvironment _env;
+        private readonly IEmailSender _emailSender;
+        public OrderController(UserManager<DbUser> userManager,
+            SignInManager<DbUser> signInManager,
+            RoleManager<DbRole> roleManager,
+            IEmailSender emailSender,
+            IWebHostEnvironment env)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _env = env;
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public IActionResult Confirm()
+        {
+          
+            return View();
+        }
+
+    }
+}
---
+++ BeeTravel/Models/BankCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeTravel.Models
+{
+    public class BankCard
+    {
+        public string Card_Number { get; set; }
+        public string Card_Holder { get; set; }
+        public string Card_CVC { get; set; }
+        public string Card_Date { get; set; }
+    }
+}
